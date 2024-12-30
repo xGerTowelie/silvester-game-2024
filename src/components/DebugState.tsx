79be83932704271d2a0ee1c0603cd06c55f9ddec
towelie <mail@towelie.dev>
2024-12-30 19:11:11 +0100

    update

--- src/components/DebugState.tsx
+++ src/components/DebugState.tsx
@@ -1,20 +1,10 @@
+
+
 "use client"
 
 import { useState } from "react";
 import { Button } from "./ui/button";
 
-function circularReplacer() {
-    const seen = new WeakSet(); // object
-    return (key, value) => {
-        if (typeof value === "object" && value !== null) {
-            if (seen.has(value)) {
-                return;
-            }
-            seen.add(value);
-        }
-        return value;
-    };
-}
 
 export default function DebugState({ title, state, disable }: { disable?: boolean, title?: string, state: unknown }) {
     const [toggle, setToggle] = useState(true)
@@ -26,7 +16,7 @@ export default function DebugState({ title, state, disable }: { disable?: boolea
     return (
         <pre className="px-5 py-3 bg-slate-900 text-white rounded shadow-sm shadow-black">
             {title && <h1 className={toggle ? "py-0" : "py-3"}>{"//"} Debugging for {title} <Button variant="secondary" size="sm" onClick={() => setToggle(prev => !prev)}>Toggle</Button></h1>}
-            {!toggle && JSON.stringify(state, circularReplacer(), 2)}
+            {!toggle && JSON.stringify(state, null, 2)}
         </pre>
     )
 }
