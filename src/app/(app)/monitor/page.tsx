b21ea8901b5b5030982783f29ac6812fb6037fc8
towelie <mail@towelie.dev>
2024-12-31 10:31:22 +0100

    reverted to hardcoded socket url

--- src/app/(app)/monitor/page.tsx
+++ src/app/(app)/monitor/page.tsx
@@ -10,10 +10,7 @@ export default function Monitor() {
     const [gameState, setGameState] = useState<GameState | null>(null)
 
     useEffect(() => {
-        const socketUrl = process.env["NEXT_PUBLIC_SOCKET_URL"]
-
-        console.log(`Trying to connect to socket: ${socketUrl}`)
-        const newSocket = io(socketUrl)
+        const newSocket = io("https://silvester-socket.towelie.dev")
         setSocket(newSocket)
 
         newSocket.on("connect", () => {
